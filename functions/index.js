5d2cfff72e6411673325885b167bc5367e51755d
Pablo Maribondo <pablomgvieira@gmail.com>
2020-06-13 14:04:32 -0300

    Cloud Functions Adding Claims

---
+++ functions/index.js
@@ -0,0 +1,21 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp();
+
+exports.addAdminRole = functions.https.onCall(async (data, context) => {
+  try {
+    const { email } = data;
+    const user = await admin.auth().getUserByEmail(email);
+    await admin.auth().setCustomUserClaims(user.id, { admin: true });
+    
+    return {
+      message: `Success! ${data.email} has been made an admin`
+    }
+  } catch (error) {
+    return {
+      message: error.message
+    }
+  }
+});
+
